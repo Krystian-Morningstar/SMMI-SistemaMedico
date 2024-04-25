65456e4b0da02a4744d521ed180c82a1578ce417
Krystian-Morningstar <zS21004519@estudiantes.uv.mx>
2024-04-25 12:17:07 -0600

    Resolución de problema de enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'; // Importa RouterModule
 
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './principal/principal.component';
+import { PrincipalComponent } from '../app/principal/principal.component';
 import { CamasComponent } from './camas/camas.component';
 import { IncidenciasComponent } from './incidencias/incidencias.component';
 
